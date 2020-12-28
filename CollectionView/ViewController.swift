f0693ae606d0c1ed6154e2e0dae8158cd74c91c7
iBubelo <iBubelo@users.noreply.github.com>
2020-12-28 17:22:57 +0100

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Ilya Bubelo on 28.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
