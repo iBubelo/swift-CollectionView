2605437b4eeb323161752d7005f04389af0af147
iBubelo <iBubelo@users.noreply.github.com>
2020-12-28 20:26:59 +0100

    add module 23 final assignment

--- CollectionView/Cell.swift
+++ CollectionView/Cell.swift
@@ -9,8 +9,16 @@ import UIKit
 class Cell: UICollectionViewCell {
     
     @IBOutlet weak var temperatureImage: UIImageView!
+    @IBOutlet weak var smileImage: UIImageView!
     
-    func setTemperatureImage(tempImage: String) {
-        temperatureImage.image = UIImage(named: tempImage)
+    func setImage(id: String, imageName: String) {
+        switch id {
+            case "temp":
+                temperatureImage.image = UIImage(named: imageName)
+            case "smile":
+                smileImage.image = UIImage(named: imageName)
+            default:
+                break
+        }
     }
 }
--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var collectionViewOne: UICollectionView!
+    @IBOutlet weak var collectionViewTwo: UICollectionView!
     
     let arrayTemperature = [
         "temp.green",
@@ -19,32 +20,71 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         "temp.red",
     ]
     
+    let arraySmiles = [
+        "favorite",
+        "nice",
+        "routine",
+        "notPleasant",
+        "bad",
+        "hate",
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         collectionViewOne.delegate = self
         collectionViewOne.dataSource = self
+        
+        collectionViewTwo.delegate = self
+        collectionViewTwo.dataSource = self
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let vc = storyboard?.instantiateViewController(withIdentifier: "ShowImageVC") as? ShowImageVC else { return }
         var currentSelectedImage: String!
         
-        currentSelectedImage = arrayTemperature[indexPath.row]
+        if collectionView == collectionViewOne{
+            currentSelectedImage = arrayTemperature[indexPath.row]
+        }
+        
+        if collectionView == collectionViewTwo{
+            currentSelectedImage = arraySmiles[indexPath.row]
+        }
+        
         vc.setImageName(name: currentSelectedImage)
         present(vc, animated: true, completion: nil)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return arrayTemperature.count
+        
+        if collectionView == collectionViewOne {
+            return arrayTemperature.count
+        }
+        
+        if collectionView == collectionViewTwo {
+            return arraySmiles.count
+        }
+        
+        return 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellOne", for: indexPath) as? Cell {
-            let imageName = arrayTemperature[indexPath.row]
-            cell.setTemperatureImage(tempImage: imageName)
-            return cell
+
+        let dataSet = [
+            (view: collectionViewOne, cell: "cellOne", images: arrayTemperature, id: "temp"),
+            (view: collectionViewTwo, cell: "cellTwo", images: arraySmiles, id: "smile")
+        ]
+        
+        for item in dataSet {
+            if collectionView == item.view {
+                if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: item.cell, for: indexPath) as? Cell {
+                    let imageName = item.images[indexPath.row]
+                    cell.setImage(id: item.id, imageName: imageName)
+                    return cell
+                }
+            }
         }
+        
         return UICollectionViewCell()
     }
 }
